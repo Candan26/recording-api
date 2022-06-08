fad99810335e32886864c8e133cb087200bd671d
cagri <cagricandan91@gmail.com>
2022-06-08 11:25:58 +0300

    Initial commit

---
+++ src/main/java/com/softavail/recordingapi/RecordingApiApplication.java
@@ -0,0 +1,13 @@
+package com.softavail.recordingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordingApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecordingApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softavail/recordingapi/RecordingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softavail.recordingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordingApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
